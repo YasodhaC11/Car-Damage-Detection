9fda411e92920d43cfd4736df44ab9788ca4f4ba
Yasodha C <yasodhac11@gmail.com>
2025-11-18 14:41:33 +0530

    model

--- streamlit_app/helper.py
+++ streamlit_app/helper.py
@@ -29,23 +29,26 @@ class CarClassifierResNet(nn.Module):
         return x
 
 def predict(image_path):
-    image=Image.open(image_path).convert("RGB")
-    transform=transforms.Compose([
+    image = Image.open(image_path).convert("RGB")
+    transform = transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])
+        transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])
     ])
-
-    image_tensor=transform(image).unsqueeze(0)
+    image_tensor = transform(image).unsqueeze(0)
 
     global trained_model
 
     if trained_model is None:
-        trained_model=CarClassifierResNet()
-        trained_model.load_state_dict(torch.load("../model/saved_model.pth"))
+        device = torch.device("cpu")  # ensure CPU
+        trained_model = CarClassifierResNet()
+        trained_model.load_state_dict(torch.load("../model/saved_model.pth", map_location=device))
+        trained_model.to(device)
         trained_model.eval()
 
+    image_tensor = image_tensor.to(next(trained_model.parameters()).device)
+
     with torch.no_grad():
-        output=trained_model(image_tensor)
-        _,predicted_class=torch.max(output,1)
-        return class_names[predicted_class.item()]
+        output = trained_model(image_tensor)
+        _, predicted_class = torch.max(output, 1)
+    return class_names[predicted_class.item()]
